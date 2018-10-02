2f5e9b5a4ccf801f17e055a6b85b110d9d5f686a
VladimirGrankin <vgran74@gmail.com>
2018-10-02 06:31:57 +0500

    3.1. Элементарный калькулятор. [#185]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,37 @@
+package ru.job4j.calculator;
+
+public class Calculator {
+	private double result;
+	
+	public void add(double first, double second) {
+		this.result = first + second;
+	}
+	
+	public double getResult() {
+		return this.result;
+	}
+	
+	public void substract(double first, double second) {
+		this.result = first - second;
+	}
+	
+	public double getResult() {
+		return this.result;
+	}
+	
+	public void div(double first, double second) {
+		this.result = first / second;
+	}
+	
+	public double getResult() {
+		retirn this.result;
+	}
+	
+	public void multiple(double first, double second) {
+		this result = first * second;
+	}
+	
+	public double getResult() {
+		return this.result;
+	}
+}
---
+++ chapter_001/src/main/java/ru/job4j/calculator/CalculatorTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CalculatorTest {
+	@Test
+	public void whenAddOnePlusThenTwo() {
+		Calculator calc = new Calculator();
+		calc.add(1D, 1D);
+		double result = calc.getResult();
+		double expected = 2D;
+		assertThat(result, is(expected));
+	}
+	
+	@Test
+	public void whenSubstractTwoOnOneThenOne {
+		Calculator calc = new Calculator();
+		calc.substract(2d, 1d);
+		double result = calc.getResult();
+		double expected = 1D;
+		assertThat(result, is(expected));
+	}
+	
+	@Test
+	public void whenDivTwoOnTwoThenOne {
+		Calculator calc = new Calculator();
+		calc.div(2d, 2d);
+		double result = calc.getResult();
+		double expected = 1D;
+		assertThat(result, is(result));
+	}
+	
+	@Testpublic void whenMultipleTwoOnOneThenTwo {
+		Calculator calc = new Calculator();
+		calc.div(2D, 1D);
+		double result = calc.getResult();
+		double expected = 2D;
+		assertThat(result, is(result));
+	}
+}
