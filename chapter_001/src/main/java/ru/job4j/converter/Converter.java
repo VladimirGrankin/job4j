d1f29c82b347dbe5f10a95b07de4e2abe761a980
VladimirGrankin <vgran74@gmail.com>
2018-10-06 23:38:50 +0500

    3.2. Currency converter Конвертер валюты

--- chapter_001/src/main/java/ru/job4j/converter/Converter.java
+++ chapter_001/src/main/java/ru/job4j/converter/Converter.java
@@ -1,4 +1,48 @@
-package ru.job4j.calculate;
+package ru.job4j.converter;
+
+/**
+ * Converter - Currency converter
+ *
+ * @author Vladimir Grankin
+ * @since 0.1
+ * @version 0.1
+* */
 
 public class Converter {
+    /**
+     * Convert Russian ruble to Euro
+     * @param value Rus ruble
+     * @return Euro
+    * */
+    public int rubleToEuro(int value) {
+        return value / 70;
+    }
+
+    /**
+     * Convert Russian ruble to US Dollar
+     * @param value Rus ruble
+     * @return US Dollar
+     * */
+    public int rubleToDollar(int value) {
+        return value / 60;
+    }
+
+    /**
+     * Convert Euro to Russian ruble
+     * @param value Euro
+     * @return Russian ruble
+     * */
+    public int euroToRuble(int value) {
+        return value * 70;
+    }
+
+    /**
+     * Convert US Dollar to Russian ruble
+     * @param value US Dollar
+     * @return Russian ruble
+     * */
+    public int dollarToRuble(int value) {
+        return value * 60;
+    }
+
 }
