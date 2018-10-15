f390625399892358abde19fc1c715392ca106030
VladimirGrankin <vgran74@gmail.com>
2018-10-15 22:49:33 +0500

    Дистанция между точками. ч2 вводим метод main и создаем два объекта Point, а также заполняем их значениями

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -14,13 +14,15 @@ public class Point { // создаем класс Point
         this.x = x; // для записи в поля используем операторы this.x
         this.y = y; // и this.y
 
-        public double distanceTo(Point that) {
+        public double distanceTo(Point that) { // это каркас метода
+            // расчитывающего расстояние между двумя точками
             return -1;
         }
 
-        public static void main(String[] args) {
-            Point a = new Point(0, 1);
-            Point b = new Point(2, 5);
+        public static void main(String[] args) {    // введем метод main
+            Point a = new Point(0, 1);  // создаем два объекта типа Point
+            Point b = new Point(2, 5);  // и заполняем их значениями x1=0; y1=1
+                                            //x2=2; y2=5
         }
     }
 }
