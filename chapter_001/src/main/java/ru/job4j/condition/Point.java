f89968228a2f8b16065f0a576fd51ec460accf35
VladimirGrankin <vgran74@gmail.com>
2018-10-15 23:09:27 +0500

    Дистанция между точками. ч3  модернизируем метод distanceTo

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -14,15 +14,24 @@ public class Point { // создаем класс Point
         this.x = x; // для записи в поля используем операторы this.x
         this.y = y; // и this.y
 
-        public double distanceTo(Point that) { // это каркас метода
-            // расчитывающего расстояние между двумя точками
+        public double distanceTo(Point that) {
+            // Точка а - это текущая, начальная точка. К ней мы обращаемся через оператор this.
+            Point a = this;
+            // Точка b - это входящая точка. К ней можно обратиться через переменную that.
+            // или для удобства мы создали новую переменную b и к ней присdоили переменную that
+            Point b = that;
+            // сделаем вывод на консоль.
+            System.out.println("x1 = " + a.x);
+            System.out.println("y1 = " + a.y);
+            System.out.println("x2 = " + b.x);
+            System.out.println("y2 = " + b.y);
             return -1;
         }
 
         public static void main(String[] args) {    // введем метод main
             Point a = new Point(0, 1);  // создаем два объекта типа Point
             Point b = new Point(2, 5);  // и заполняем их значениями x1=0; y1=1
-                                            //x2=2; y2=5
+            a.distanceTo(b);                                //x2=2; y2=5
         }
     }
 }
