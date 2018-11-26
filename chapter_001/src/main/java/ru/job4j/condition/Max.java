526b5988e4b0de50635d000c9f4988f45f888783
RM work <vgran74@hotmail.com>
2018-11-26 09:52:50 +0500

    4.4 Максимум из 3 чисел

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -15,4 +15,7 @@ public class Max {
     public int max(int first, int second) {
         return (first > second) ? first : second;
     }
+    public int max(int first, int second, int third) {
+        return max(first,(max(second,third)));
+    }
 }
