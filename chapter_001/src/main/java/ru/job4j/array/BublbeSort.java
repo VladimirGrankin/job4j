50abf45c185f5f316956341fac46fcac628e7110
VladimirGrankin <vgran74@gmail.com>
2019-02-05 00:00:20 +0500

    temp2

--- chapter_001/src/main/java/ru/job4j/array/BublbeSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BublbeSort.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class BublbeSort {
     public int[] sort(int[] array) {
         for (int i = 0; i != array.length; i++) {
-            if (array[i] > array[i+1] {
+            if (array[i] > array[i+1]) {
                     // меняем местами правый и левый элемент массива
                 int tmp = array[i];
                 array[i] = array[i+1];
@@ -11,6 +11,7 @@ public class BublbeSort {
 
             }
         }
+        return array;
     }
 
 }
