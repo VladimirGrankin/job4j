c2c4d1e04c6a4d7b5d429e9ccd00defc32ef8275
VladimirGrankin <vgran74@gmail.com>
2019-02-14 18:46:33 +0600

    6.5  Программа для сортировки массива методом перестановки

---
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+/**
+ * Клвсс BubbleSort сортирует массив методом пузырьковой сортировки
+ * @author Vladimir Grankin
+ * @since 0.1
+ * @version 0.2
+ */
+public class BubbleSort {
+    /**
+     * Метод sort перебирает и сравнивает элем. массива, и, если левый элемент больше правого, меняет их местами
+     * @param array - сортируемый массив
+     * @return - возвращает отсортированный массив
+     */
+    public int[] sort(int[] array) {
+        for (int i = 0; i != array.length; i++) {
+            if (array[i] > array[i + 1]) {
+                int tmp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = tmp;
+            }
+        }
+        return array;
+    }
+}
--- chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -1,5 +1,20 @@
 package ru.job4j.array;
 
-public class BubbleSortTest {
+import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class BubbleSortTest {
+    /**
+     * Метод тестирует пузырьковую сортировку
+     */
+    @Test
+    public void whenSortArrayWithTenElementsThenSortedArray() {
+        BubbleSort bubbles = new BubbleSort();
+        int[] input = new int[] {10, 2, 9, 4, 7, 5, 6, 8, 3, 1};
+        int[] result = bubbles.sort(input);
+        int[] expect = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        assertThat(result, is(expect));
+    }
 }
